4b64b2a11b6126e908c09627897ed64380198ea0
SimoGianna <simone.giannasi@itiscuneo.eu>
2024-09-26 17:16:25 +0200

    Funzionante

--- ServerUDP.py
+++ ServerUDP.py
@@ -1,36 +1,50 @@
 import socket
+import threading
 
 # Definisce l'indirizzo IP e la porta del server
-server_ip = "192.168.141.164"
-server_port = 12345
-buffer_size = 1024
+server_ip = "127.0.0.1"  # Cambia in "localhost" se necessario
+server_port = 12346
+buffer_size = 4092
 server_address = (server_ip, server_port)
 
-
-
 # Crea un socket UDP
-udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #SOCK_DGRAM = socket UDP
+udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Associa l'indirizzo e la porta al socket
 udp_server_socket.bind(server_address)
 
+client_address = None
 
-while True:
-    print(f"Server in ascolto su {server_ip}:{server_port}...")
-    data, client_address = udp_server_socket.recvfrom(buffer_size) #bloccante
-    print(f"Messaggio ricevuto: {data.decode()} da {client_address}")
-    response = "Messaggio ricevuto"
-    udp_server_socket.sendto(response.encode(), client_address)
+# Funzione che aspetta la ricezione del messaggio dal client
+def riceviMessaggi():
+    global client_address
+    while True:
+        try:
+            data, client_address = udp_server_socket.recvfrom(buffer_size)  # Bloccante
+            print(f"Client: {data.decode()}")
+        except Exception as e:
+            print(f"Errore: {e}")
 
+# Funzione che invia i messaggi 
+def inviaMessaggi():
+    while True:
+        try:
+            if client_address:
+                messaggio = input("Server: ")
+                udp_server_socket.sendto(messaggio.encode(), client_address)
+        except Exception as e:
+            print(f"Errore: {e}")
 
-udp_server_socket.close() #chiude la trasmissione (devo togliere il while true)
+# Creo i threads
+thread_ricevitore = threading.Thread(target=riceviMessaggi)
+thread_inviatore = threading.Thread(target=inviaMessaggi)
 
+# Avvio i threads
+thread_ricevitore.start()
+thread_inviatore.start()
 
+# Aspetto che i thread finiscano (questo potrebbe non accadere mai in questo caso)
+thread_ricevitore.join()
+thread_inviatore.join()
 
-
-
-
-
-
-
-
-
+# Chiusura del socket
+udp_server_socket.close()
